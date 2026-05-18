2dd82ef07babdf88b6145aa986c77c8578d5f28a
gulaitapidigoreng <ghifari.shinichi31@gmail.com>
2026-05-18 21:43:57 +0700

    Extracted RenderWorld function into its own class, so that because its has different purpose from Program.cs

---
+++ src/LifeSim/Core/Renderer.cs
@@ -0,0 +1,44 @@
+﻿using LifeSim.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSim.Core
+{
+    public static class Renderer
+    {
+        public static void RenderWorld(World world)
+        {
+            Console.SetCursorPosition(0, 0);
+
+            var plants = world.All.OfType<Plant>().Count();
+            var herbs = world.All.OfType<Herbivore>().Count();
+            var preds = world.All.OfType<Predator>().Count();
+
+            Console.ResetColor();
+            Console.WriteLine($"Tick: {world.Tick,-8}  Plants: {plants,-5}  Herbivores: {herbs,-5}  Predators: {preds,-5}   [Space/P] pause, [Q/Esc] quit");
+
+            var snapshot = world.GridSnapshot();
+            for (var y = 0; y < world.Height; y++)
+            {
+                for (var x = 0; x < world.Width; x++)
+                {
+                    if (snapshot.TryGetValue(new Point2(x, y), out var organism))
+                    {
+                        organism.ApplyColor();
+                        Console.Write(organism.Glyph);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
